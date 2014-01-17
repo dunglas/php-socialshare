51bcc42b33ecd5764c8142771f6dc807e34d0e48
Morrison Laju <morrelinko@gmail.com>
2014-01-17 03:08:37 +0100

    Make use of `NAME` constant to specify the provider

--- examples/buttons.php
+++ examples/buttons.php
@@ -20,25 +20,25 @@ $socialShare->registerProvider(new Pinterest());
 
 <ul>
     <li>
-        <a href="<?php echo htmlspecialchars($socialShare->getLink('facebook', 'http://dunglas.fr')) ?>">
-            Share on Facebook (<?php echo $socialShare->getShares('facebook', 'http://dunglas.fr') ?>)
+        <a href="<?php echo htmlspecialchars($socialShare->getLink(Facebook::NAME, 'http://dunglas.fr')) ?>">
+            Share on Facebook (<?php echo $socialShare->getShares(Facebook::NAME, 'http://dunglas.fr') ?>)
         </a>
     </li>
 
     <li>
-        <a href="<?php echo htmlspecialchars($socialShare->getLink('twitter', 'http://dunglas.fr', array('via' => '@dunglas', 'text' => 'Kévin Dunglas\' SocialShare library'))) ?>">
-            Share on Twitter (<?php echo $socialShare->getShares('twitter', 'http://dunglas.fr') ?>)
+        <a href="<?php echo htmlspecialchars($socialShare->getLink(Twitter::NAME, 'http://dunglas.fr', array('via' => '@dunglas', 'text' => 'Kévin Dunglas\' SocialShare library'))) ?>">
+            Share on Twitter (<?php echo $socialShare->getShares(Twitter::NAME, 'http://dunglas.fr') ?>)
         </a>
     </li>
 
     <li>
-        <a href="<?php echo htmlspecialchars($socialShare->getLink('google', 'http://dunglas.fr')) ?>">
-            Share on Google Plus (<?php echo $socialShare->getShares('google', 'http://dunglas.fr') ?>)
+        <a href="<?php echo htmlspecialchars($socialShare->getLink(Google::NAME, 'http://dunglas.fr')) ?>">
+            Share on Google Plus (<?php echo $socialShare->getShares(Google::NAME, 'http://dunglas.fr') ?>)
         </a>
     </li>
     <li>
-        <a href="<?php echo htmlspecialchars($socialShare->getLink('pinterest', 'http://dunglas.fr', array('description' => 'Kévin\'s avatar', 'http://dunglas.fr/wp-content/uploads/2008/03/191x300xkeyes-191x300.png.pagespeed.ic.weNKMj-Pq4.png'))) ?>">
-            Share on Pinterest (<?php echo $socialShare->getShares('pinterest', 'http://dunglas.fr') ?>)
+        <a href="<?php echo htmlspecialchars($socialShare->getLink(Pinterest::NAME, 'http://dunglas.fr', array('description' => 'Kévin\'s avatar', 'http://dunglas.fr/wp-content/uploads/2008/03/191x300xkeyes-191x300.png.pagespeed.ic.weNKMj-Pq4.png'))) ?>">
+            Share on Pinterest (<?php echo $socialShare->getShares(Pinterest::NAME, 'http://dunglas.fr') ?>)
         </a>
     </li>
 </ul>
